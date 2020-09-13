38abc70a6108107021f9b4aa357830a03ba170f1
Tommy <tommyfenv@163.com>
2020-09-13 21:51:30 +0800

    增加网络请求相关案例

---
+++ AndroidHttpSample/app/src/main/java/me/tommy/androidhttpsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.tommy.androidhttpsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
